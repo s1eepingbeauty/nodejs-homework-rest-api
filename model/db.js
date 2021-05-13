a7ed1ca2d9ce0d28722409e9d6dcae15c70832c2
Elena Mazurenko <kingz.bridge@gmail.com>
2021-05-13 19:46:00 +0300

    created contactSchema

--- model/db.js
+++ model/db.js
@@ -1,18 +1,32 @@
-const { MongoClient } = require('mongodb')
+const mongoose = require('mongoose')
 require('dotenv').config()
 const uriDb = process.env.URI_DB
 
-const db = MongoClient.connect(uriDb, {
+const db = mongoose.connect(uriDb, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true, // индекс нужен для ускоренного поиска, но создание новых записей может происходить дольше
   poolSize: 5,
 })
 
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to DB')
+})
+
+mongoose.connection.on('error', error => {
+  console.log(`Mongoose connection error: ${error.message}`)
+})
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected')
+})
+
 process.on('SIGINT', async () => {
-  const client = await db
-  client.close()
-  console.log('Disconnect MongoDB')
-  process.exit(1)
+  // отслеживаем нажатие Ctrl+c
+  mongoose.connection.close(() => {
+    console.log('Disconnect MongoDB')
+    process.exit(1)
+  })
 })
 
 module.exports = db
