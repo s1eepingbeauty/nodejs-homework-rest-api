219259b2a1951bca1a0846baa6a9d9d08a8c2e0b
Elena Mazurenko <kingz.bridge@gmail.com>
2021-06-04 21:21:46 +0300

    prepare multer to upload files

--- helpers/upload.js
+++ helpers/upload.js
@@ -1,2 +1,37 @@
 const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+require('dotenv').config()
+
+const UPLOAD_DIR = process.env.UPLOAD_DIR
+
+const storage = multer.diskStorage({
+  // в какую папку положить
+  destination: function (req, file, cb) {
+    cb(null, UPLOAD_DIR)
+  },
+  // как назвать файл
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now().toString()}-${file.originalname}`)
+  },
+})
+// максимальный размер аватарки 2 Mb
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 2000000 },
+  fileFilter: (req, file, cb) => {
+    // Функция должна вызывать `cb` с булевым значением,
+    // которое показывает, следует ли принять файл
+
+    // Если не картинка - отклонить файл
+    if (!file.mimetype.includes('image')) {
+      cb(null, false)
+      return
+    }
+    // Принять файл
+    cb(null, true)
+
+    // Вы можете всегда вернуть ошибку, если что-то пошло не так:
+    cb(new Error("I don't have a clue!"))
+  },
+})
+
+module.exports = upload
